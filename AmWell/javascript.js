2a51e2fd241545a9b845080dab238af7b908e389
yonatanmk <yonatanmk@gmail.com>
2020-10-15 15:27:06 -0400

    remove styling and cleanup

--- AmWell/javascript.js
+++ AmWell/javascript.js
@@ -13,30 +13,14 @@ require([
       style: 'bottom:0px'
     }, 'form');
     getValues = function () {
-      console.log(form.get('value'))
       values = form.get('value').state
-      console.log(values)
       const dropdownButton = query("#dijit_form_ComboButton_0_arrow")[0];
-      const style = dropdownButton.style
       if (values[0]) {
-        // style['background'] = "#1774cc"
-        // style['color'] = "white"
-        // style['border'] = "2px solid #1774cc"
         domClass.add('dijit_form_ComboButton_0_arrow', "dropdownButtonFilled")
       } else {
         domClass.remove('dijit_form_ComboButton_0_arrow', "dropdownButtonFilled")
-        // debugger;
-        // style['background'] = ""
-        // style['color'] = ""
-        // style['border'] = ""
       }
     };
-    onClick = function () {
-      console.log('On Click')
-    };
-    onShow = function () {
-      console.log('On Show')
-    }
     new CheckedMultiSelect({
       id: 'dropdown-multiselect',
       dropDown: true,
@@ -53,7 +37,5 @@ require([
     }, 'form');
     form.startup();
     var dropdownButton = query("#dijit_form_ComboButton_0_arrow")
-    console.log('DORPDOWN')
-    console.log(dropdownButton)
-    dropdownButton[0].innerHTML = '<div class="dropdownButtonContent"><p class="dropdownButtonText">Languages</p><div class="dropdownChevron"></div></div>'
+    dropdownButton[0].innerHTML = '<div class="dropdownButtonContent"><p class="dropdownButtonText">Language</p><div class="dropdownChevron"></div></div>'
 });
