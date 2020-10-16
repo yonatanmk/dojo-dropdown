7bcc1d2f4ad0b9c53e859d45de9deb105b672748
yonatanmk <yonatanmk@gmail.com>
2020-10-16 11:49:27 -0400

    second button cleanup

--- AmWell/javascript.js
+++ AmWell/javascript.js
@@ -8,34 +8,50 @@ require([
   "dojo/parser",
   "dojo/domReady!"], function (query, construct, JSON, Form, CheckedMultiSelect, domClass) {
     let values = [];
-    var form, div = construct.create('div', {
+    var form = construct.create('div', {
       'class': 'pad',
       style: 'bottom:0px'
     }, 'form');
-    getValues = function () {
+    getStateValues = function () {
       values = form.get('value').state
-      const dropdownButton = query("#dijit_form_ComboButton_0_arrow")[0];
       if (values[0]) {
         domClass.add('dijit_form_ComboButton_0_arrow', "dropdownButtonFilled")
       } else {
         domClass.remove('dijit_form_ComboButton_0_arrow', "dropdownButtonFilled")
       }
     };
+    getCountryValues = function () {
+      values = form.get('value').country
+      if (values[0]) {
+        domClass.add('dijit_form_ComboButton_1_arrow', "dropdownButtonFilled")
+      } else {
+        domClass.remove('dijit_form_ComboButton_1_arrow', "dropdownButtonFilled")
+      }
+    };
     new CheckedMultiSelect({
-      id: 'dropdown-multiselect',
+      class: 'dropdownMultiselect',
       dropDown: true,
       labelText: 'States',
       multiple: true,
       name: 'state',
-      onChange: getValues,
-      onClick: onClick,
-      onShow: onShow,
+      onChange: getStateValues,
       required: false
     }, "stateSelect");
+    new CheckedMultiSelect({
+      class: 'dropdownMultiselect',
+      dropDown: true,
+      labelText: 'Countries',
+      multiple: true,
+      name: 'country',
+      onChange: getCountryValues,
+      required: false
+    }, "countrySelect");
     form = new Form({
       style: 'display:block'
     }, 'form');
     form.startup();
-    var dropdownButton = query("#dijit_form_ComboButton_0_arrow")
-    dropdownButton[0].innerHTML = '<div class="dropdownButtonContent"><p class="dropdownButtonText">Language</p><div class="dropdownChevron"></div></div>'
+    var stateButton = query("#dijit_form_ComboButton_0_arrow")
+    stateButton[0].innerHTML = '<div class="dropdownButtonContent"><p class="dropdownButtonText">State</p><div class="dropdownChevron"></div></div>'
+    var countryButton = query("#dijit_form_ComboButton_1_arrow")
+    countryButton[0].innerHTML = '<div class="dropdownButtonContent"><p class="dropdownButtonText">Country</p><div class="dropdownChevron"></div></div>'
 });
